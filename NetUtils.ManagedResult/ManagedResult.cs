6ed09ad1e26058d221ccd516dd083aba06810981
Juraj Ahel <juraj.ahel@gmail.com>
2024-07-10 14:29:23 +0200

    Add project files.

---
+++ NetUtils.ManagedResult/ManagedResult.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace NetUtils.ManagedResult;
+
+[DebuggerStepThrough]
+public record ManagedResult<T>(bool Success, List<string> Issues, T? Result)
+{
+    public bool Fail => !Success;
+    public string IssuesTextAggregate => string.Join("\n", Issues);
+
+    public static ManagedResult<T> Successful(T? result)
+    {
+        return new ManagedResult<T>(true, new List<string>(), result);
+    }
+
+    public static ManagedResult<T> Failed(List<string> issues)
+    {
+        return new ManagedResult<T>(false, issues, default);
+    }
+
+    public static ManagedResult<T> Failed(string issue)
+    {
+        return new ManagedResult<T>(false, new List<string> {issue}, default);
+    }
+
+    public ManagedResult(T? result) : this(true, new List<string>(), result)
+    {
+    }
+
+    public ManagedResult(bool success, string issue) : this(success, new List<string> { issue }, default)
+    {
+    }
+
+    public ManagedResult(bool success, List<string> issues) : this(success, issues, default)
+    {
+    }
+}
