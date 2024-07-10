03f9106a0148e657e45f9f568a6e42012b7585e0
Juraj Ahel <juraj.ahel@gmail.com>
2024-07-10 14:29:23 +0200

    Add project files.

---
+++ NetUtils.ManagedResult/ManagedResultFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace NetUtils.ManagedResult;
+
+[DebuggerStepThrough]
+public static class ManagedResultFactory
+{
+    public static ManagedResult<T> Successful<T>(T? result) => ManagedResult<T>.Successful(result);
+    public static ManagedResult<T> Failed<T>(string issue) => ManagedResult<T>.Failed(new List<string>{issue});
+    public static ManagedResult<T> Failed<T>(List<string> issues) => ManagedResult<T>.Failed(issues);
+    public static ManagedResult<T> Failed<T>(string issue, List<string> inheritedIssues) => ManagedResult<T>.Failed(new List<string>(){issue}.Concat(inheritedIssues).ToList());
+
+    //public static object Failed<T>(List<string> issues, Type type)
+    //{
+    //    // Get the generic type definition
+    //    Type genericType = typeof(ManagedResult<>);
+
+    //    // Make the generic type with the provided type argument
+    //    Type constructedType = genericType.MakeGenericType(type);
+
+    //    // Create an instance of the constructed type
+    //    return Activator.CreateInstance(constructedType;
+
+    //}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+![Status](https://github.com/xeniorn/NetUtils.ManagedResult/actions/workflows/dotnet.yml/badge.svg?branch=main)
+
+Utility classes for returning result objects encapsulating success, result, and issues.
